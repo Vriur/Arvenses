9bddd7a3b1738ad61e491df1653f0a7b30cb1bfc
Nathan González H <nathgh04@gmail.com>
2023-10-04 13:48:00 -0600

    Database update, Help constants change & database service init function

--- App.js
+++ App.js
@@ -3,8 +3,11 @@ import { MenuProvider } from 'react-native-popup-menu';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationStack from './src/navegation/NavegationStack';
+import { initDatabase } from './database-service';
 
 function App() {
+  initDatabase();
+  
   return (
     <SafeAreaProvider>
       <MenuProvider>
--- database-service.js
+++ database-service.js
@@ -2,15 +2,25 @@ import * as SQLite from "expo-sqlite";
 import * as FileSystem from "expo-file-system";
 import { Asset } from "expo-asset";
 
+export async function initDatabase() {
+    if((await FileSystem.getInfoAsync(FileSystem.documentDirectory + "SQLite/arvenses_db.db")).exists) {
+        await FileSystem.deleteAsync(FileSystem.documentDirectory + "SQLite/arvenses_db.db");
+    }
+}
+
 export async function openDatabase() {
-    if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+    if(!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
         await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
     }
 
-    await FileSystem.downloadAsync(
+    if(!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + "SQLite/arvenses_db.db")).exists) {
+        await FileSystem.downloadAsync(Asset.fromModule(require("./src/assets/database/arvenses_db.db")).uri, FileSystem.documentDirectory + "SQLite/arvenses_db.db");
+    }
+
+    /*await FileSystem.downloadAsync(
         Asset.fromModule(require('./src/assets/database/arvenses_db.db')).uri, 
         FileSystem.documentDirectory + 'SQLite/arvenses_db.db'
-    );
+    );*/
 
     return SQLite.openDatabase('arvenses_db.db');
 }
