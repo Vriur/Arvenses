cc8ff499f4df3933fe25d897ab96ad582993a026
Nathan González H <nathgh04@gmail.com>
2025-02-16 18:30:02 -0600

    fix version upgrade

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.assetExts.push('db');
+defaultConfig.transformer = {
+    ...defaultConfig.transformer,
+    _expoRelativeProjectRoot: __dirname
+  }
 module.exports = defaultConfig;
--- src/screens/AditionalInformation.js
+++ src/screens/AditionalInformation.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ADITIONAL_INFORMATION } from '../../Constants';
 import { AntDesign } from '@expo/vector-icons'; 
+
+import * as FileSystem from 'expo-file-system';
+import * as IntentLauncher from 'expo-intent-launcher';
+import { Asset } from 'expo-asset';
+import * as Sharing from 'expo-sharing';
 
 const styles = StyleSheet.create({
     container: {
@@ -32,11 +37,28 @@ const styles = StyleSheet.create({
 });
 
 const AditionalInformation = ({navigation}) => {
+    const loadFile = async (uri) => {
+        const [{ localUri }] = await Asset.loadAsync(uri);
+        FileSystem.getContentUriAsync(localUri).then(contentUri => {
+            if(Platform.OS === 'android'){
+                IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
+                    data: contentUri,
+                    flags: 1,
+                })
+            }
+            else{
+                if(Platform.OS === 'ios'){
+                    Sharing.shareAsync(contentUri);
+                }
+            }
+        });
+    }
+
     return(
         <View style = {styles.container}>
             <FlatList data = {ADITIONAL_INFORMATION.BUTTONS}
                 renderItem = {({item}) => 
-                    <TouchableOpacity style = {styles.button} onPress = {() => navigation.navigate('PdfReader', {url: item.DOWNLOADABLE_FILE})}>
+                    <TouchableOpacity style = {styles.button} onPress = {() => loadFile(item.DOWNLOADABLE_FILE)}>
                         <Text style = {styles.buttonText}>{item.TEXT}</Text>
                         <View style = {styles.spaceFiller} />
                         <AntDesign name = 'download' size = {30} color = '#174c72' />
